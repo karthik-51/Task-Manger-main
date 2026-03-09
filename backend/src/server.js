2c046873968e5791bbae0d3fce1b21e3aa70cdf1
Karthik Reddy <145337066+karthik-51@users.noreply.github.com>
2026-03-09 20:01:53 +0530

    Update server.js
    hello

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,4 @@
-// require("dotenv").config();
-// const app = require("./app");
-// const { connectDB } = require("./config/database");
 
-// connectDB(process.env.MONGO_URI);
-
-// app.listen(process.env.PORT, () =>
-//   console.log(`Server running on port ${process.env.PORT}`)
-// );
 
 require("dotenv").config();
 const app = require("./app");
